ea0fff1ea100022888e8f41ce8966cb3cd868cfc
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 17:38:04 +0530

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,19 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const arr3 = [];
+
+    for(let i =0; i<arr1.length;i++) {
+        let inArr2 = false;
+        for(let j=0;j<arr2.length;j++) {
+            if(arr1[i] === arr2[j]) {
+                inArr2 = true;
+            }
+        }
+        if(inArr2) {
+            arr3.push(arr1[i])
+        }
+    }
+
+    return arr3
+}
 
 module.exports = arrayIntersection;

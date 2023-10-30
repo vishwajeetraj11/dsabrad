71a83741e6ab7d7e873ca5696a630bc4291d80b0
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 22:19:45 +0530

    all characters unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const freqency = {};
+    let flag = true;
+    for(let i = 0; i<str.length; i++) {
+        if(freqency[str[i]]) {
+            flag = false;
+            break
+        } else {
+            freqency[str[i]] = 1
+        }
+    }
+    return flag;
+
+}
+
+console.log(areAllCharactersUnique('abc'))
 
 module.exports = areAllCharactersUnique;

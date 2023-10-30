e02278c0d974e0b3529ecfef46506c85b8a4ea61
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 17:58:14 +0530

    display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(arr) {
+    const numUsers = arr.length;
+    let likeStatement = '';
+    if(numUsers === 0) {
+        likeStatement = 'no one likes this'
+    } else if (numUsers === 1) {
+        likeStatement = `${arr[0]} likes this`
+    } else if (numUsers === 2) {
+        likeStatement = `${arr[0]} and ${arr[1]} like this`
+    } else if (numUsers === 3) {
+        likeStatement = `${arr[0]}, ${arr[1]} and ${arr[2]} like this`
+    } else {
+        likeStatement = `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`
+    }
+    return likeStatement
+}
 
 module.exports = displayLikes;

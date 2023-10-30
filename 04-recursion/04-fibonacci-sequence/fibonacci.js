941d10dca7a9765c3815c137329d27176c394d56
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 01:24:44 +0530

    recursion: fibonacci

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,7 @@
-function fibonacci() {}
+function fibonacci(num) {
+    if(num === 1) return 1;
+    if(num === 0) return 0;
+    return fibonacci(num-1)+fibonacci(num-2)
+}
 
 module.exports = fibonacci;

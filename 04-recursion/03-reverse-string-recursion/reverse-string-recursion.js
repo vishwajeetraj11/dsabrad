8272b622efe58e63fa472a5f37c737c6e57dae0d
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 01:20:32 +0530

    recursion: reverse string

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,8 @@
-function reverseString() {}
+function reverseString(str) {
+    if(str.length === 1) {
+        return str[0]
+    }
+    return str[str.length-1] + reverseString(str.slice(0,str.length-1))
+}
 
 module.exports = reverseString;

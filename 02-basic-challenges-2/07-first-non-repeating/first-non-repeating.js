22a3dc7ae90b8f5cb05ddb99ca7b972a33eba975
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 22:57:31 +0530

    first non repeating char

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,23 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    let frequency = {}
+
+    for(let i =0; i<str.length; i++) {
+        if(frequency[str[i]]) {
+            frequency[str[i]] = frequency[str[i]] + 1;
+        } else {
+            frequency[str[i]] = 1
+        }
+    }
+
+    for(let i=0;i<str.length;i++) {
+        if(frequency[str[i]] === 1) {
+            return str[i]
+        }
+    }
+
+    return null
+}
+
+// console.log(findFirstNonRepeatingCharacter('aa,ccd'))
 
 module.exports = findFirstNonRepeatingCharacter;

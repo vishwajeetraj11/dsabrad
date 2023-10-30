c586c6821714429aefa2da72de154c68775a2752
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 17:18:42 +0530

    fizz buzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,22 @@
-function fizzBuzzArray() {}
+
+
+function fizzBuzzArray(arrlength) {
+    const newArr = []
+    for(let i=1; i<=arrlength; i++) {
+       
+        if(i % 3 === 0 && i % 5 === 0) {
+            newArr.push('FizzBuzz')
+        }
+        else if(i % 3 === 0) {
+            newArr.push('Fizz')
+        } else if (i % 5 === 0) {
+            newArr.push('Buzz')
+        } else {
+            newArr.push(i)
+        }
+    }
+    return newArr
+}
+
 
 module.exports = fizzBuzzArray;

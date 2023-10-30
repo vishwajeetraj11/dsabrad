4aea92ffd588f6f82c3f12f72d8fd2699021ec0d
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 15:13:27 +0530

    title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(string) {
+    const arr = string.split(' ');
+    for(let i =0;i<arr.length;i++) {
+        let word = arr[i];
+        word = word[0].toUpperCase() + word.slice(1);
+        console.log(word)
+        arr[i] = word 
+    }
+    return arr.join(' ');
+}
 
 module.exports = titleCase;

6badab2c5ed4e4c6d22da948cf5c72a172b6d243
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 16:52:24 +0530

    duplicates in array

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,32 @@
-function removeDuplicates() {}
+
+const isItemInArray = (arr, item) => {
+    for(let j=0;j<arr.length;j++) {
+        if(arr[j] === item) {
+            return true
+        }
+    }
+    return false
+}
+
+function removeDuplicates(arr) {
+   const unique = [arr[0]];
+   for(let i = 0; i<arr.length; i++) {
+    const currentItem = arr[i]
+
+    let notInUnique = true;
+    for(let j=0;j<unique.length;j++) {
+        if(unique[j] === currentItem) {
+            notInUnique = false;
+        }
+    }
+    if(notInUnique) {
+        unique.push(currentItem)
+    }
+   }
+   return unique
+}
+
+removeDuplicates([11,22,33,33])
 
 module.exports = removeDuplicates;
+

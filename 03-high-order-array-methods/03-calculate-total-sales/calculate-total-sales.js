206367276f1ccb311dc121238e79b3c4046dea10
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 23:59:38 +0530

    total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,7 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+    const totalSales = products.reduce((total, current) => total+(current.price*current.quantity), 0);
+    const tax = totalSales*(taxRate / 100);
+    return tax+totalSales
+}
 
 module.exports = calculateTotalSalesWithTax;

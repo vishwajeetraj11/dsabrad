8a814d252671f9fa029bd2ff05dec76c20b32211
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 00:10:54 +0530

    highest Score

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,19 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+    const arr = str.split(' ');
+    const perWordScoreArr = arr.map(word => getWordScore(word));
+    const maxScore = Math.max(...perWordScoreArr);
+    const indexOfMaxScore = perWordScoreArr.findIndex((el) => el === maxScore);
+    return arr[indexOfMaxScore];
+}
+
+const getWordScore = (word) => {
+    const arr = word.split('');
+    const scoreArr = arr.map(letter => letter.charCodeAt()-96);
+    const score = scoreArr.reduce((score, curr) => score+curr, 0)
+    return score;
+}
+
+// console.log(highestScoringWord('hello my name is xavier'))
+
 
 module.exports = highestScoringWord;

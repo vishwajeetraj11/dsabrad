92a7e05247fbab3f8fa605d0846b656db9031fe0
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 01:36:36 +0530

    recursion: number range

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,6 @@
-function numberRange() {}
+function numberRange(start,end) {
+    if(start===end) return [end];
+    return [start].concat(numberRange(start+1, end))
+}
 
 module.exports = numberRange;

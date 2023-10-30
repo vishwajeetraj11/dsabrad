8d45ffd3f78186ef495f3e1b76757e0c6e7d21de
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 14:48:56 +0530

    reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,17 @@
-function reverseString() {}
+function reverseString(string) {
+    // 1.
+    // const reversed = string.split('').reverse().join('');
+    // return reversed;
+
+    // 2.
+    let reversed = '';
+    for(let i =0;i<string.length;i++) {
+        console.log(string[string.length -1- i])
+        reversed = reversed + string[string.length -1- i]
+    }
+    return reversed
+}
 
 module.exports = reverseString;
+
+console.log(reverseString('apples'))

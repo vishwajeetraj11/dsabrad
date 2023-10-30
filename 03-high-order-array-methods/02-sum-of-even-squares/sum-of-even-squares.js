a03d606fb1276ff3c724852d10b5c2c4df044af5
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 23:47:15 +0530

    sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    let sum = 0;
+    arr.forEach(num => {
+        if(num%2===0) {
+            sum = sum+num*num
+        }
+    })
+    return sum;
+}
 
 module.exports = sumOfEvenSquares;

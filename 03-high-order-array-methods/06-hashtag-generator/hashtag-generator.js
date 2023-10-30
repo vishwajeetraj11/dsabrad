e71935c7313de0089c92adfeea2c3bc4775d3890
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 00:55:07 +0530

    gnerate hash

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,11 @@
-function generateHashtag() {}
+function generateHashtag(string) {
+    if(string.length > 140 || string.length ===0) return false;
+    // const updatedString = string.replace(' ', '');
+    return `#${string.split(' ').filter(Boolean).map(word => {
+            return word[0].toUpperCase()+word.slice(1)
+    }).join('')}`
+}
 
 module.exports = generateHashtag;
+
+// console.log(generateHashtag(' Hello there thanks for trying my Kata'))

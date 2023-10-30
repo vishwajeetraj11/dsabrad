84ea8dbc57d07daf63eb387aee7de9b0460d35b4
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 15:05:55 +0530

    finding max no.

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    let max=0;
+    for(let i = 0; i<arr.length ; i++) {
+        if(max<arr[i]) {
+            max = arr[i]
+        }
+    }
+    return max;
+}
 
 module.exports = findMaxNumber;

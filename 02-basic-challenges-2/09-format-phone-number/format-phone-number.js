49e13a8c7e8ad9ed527df02c0a807ef447b9172f
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 23:36:25 +0530

    formatted phone no.

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,7 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+    // return `(${arr.slice(0,3).join('')}) ${arr.slice(3,6).join('')}-${arr.slice(6).join('')}`
+    const strNum = arr.join('')
+    return `(${strNum.substring(0,3)}) ${strNum.substring(3,6)}-${strNum.substring(6)}`
+}
 
 module.exports = formatPhoneNumber;

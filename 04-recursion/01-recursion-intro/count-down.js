0ff7b39b4f30844fd2eaec4efb34ec8c629b4708
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 01:14:56 +0530

    count down

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,18 @@
-function countDown() {}
+function countDown(num) {
+    if(num <= 0) {
+        console.log('All done!')
+        return;
+    }
+
+    console.log(num);
+    countDown(num-1)
+   
+    // if(num <= 0) {
+    //     console.log('All done!')
+    //     return;
+    // }
+    // console.log(num);
+    // countDown(num-1)
+}
 
 module.exports = countDown;

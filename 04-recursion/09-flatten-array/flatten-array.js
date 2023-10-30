96f1045c4edaad882336a760cf70c5c7be712411
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 01:50:40 +0530

    flaten array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,17 @@
-function flattenArray() {}
+
+function flattenArray(arr) {
+
+    let result = []
+
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] instanceof Array) {
+            result = result.concat(flattenArray(arr[i]))
+        } else {
+             result.push(arr[i])
+        }
+    }
+    return result
+
+}
 
 module.exports = flattenArray;

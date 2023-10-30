07f88372599eb50d3edfebd1b7bdc8f941d35454
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 18:13:02 +0530

    find missing no.

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,12 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    const length = arr.length+1;
+    const expectedSum = (length*(length+1))/2;
+    let currentSum = 0;
+    
+    for(let i = 0; i<arr.length;i++) {
+        currentSum = currentSum+arr[i];
+    }
+    return expectedSum-currentSum
+}
 
 module.exports = findMissingNumber;

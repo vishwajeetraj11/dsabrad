f83fc1034f0851d4835c051e8f54d1fa482a500b
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 14:55:25 +0530

    count frequency of letter in string.

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+// function countOccurrences(string, char) {
+//   let frequency = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === char) {
+//       frequency++;
+//     }
+//   }
+//   return frequency;
+// }
+
+const countOccurrences = (string, char) => string.split(char).length-1
 
 module.exports = countOccurrences;
+
+console.log(countOccurrences("apples", "p"));

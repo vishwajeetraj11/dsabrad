d342a0e6511c580fd867295ae4ecdc37b3e84b10
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 15:24:11 +0530

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,15 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    let newString = string.split('').filter(str => str !== ' ' && str !== ',').join('');
+    console.log(newString)
+    for(let i=0;i<newString.length;i++) {
+        if(newString[i].toLowerCase() !== newString[newString.length-i-1].toLowerCase()) {
+            return false;
+        }
+    }
+    return true
+}
+
+console.log(isPalindrome('A man, a plan, a canal, Panama'))
+
 
 module.exports = isPalindrome;

c60616b7ecb88de5265953c12838edcd83c23e16
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 15:32:52 +0530

    count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(string) {
+    let vowelsCount =0;
+    let _string = string.toLowerCase()
+    for(let i=0;i<string.length;i++) {
+        if(_string[i]==='a'||_string[i]==='e'||_string[i]==='i'||_string[i]==='o'||_string[i]==='u') {
+            vowelsCount++;
+        }
+    }
+    return vowelsCount
+}
 
 module.exports = countVowels;

abab56455b8c8214e4a3fedb6446a0aa6224cce9
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-30 21:48:43 +0530

    find missing letters

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+function findMissingLetter(letterArr) {
+    // const letterArr = string.split('');
+    const charCodeArr = letterArr.map(e => e.charCodeAt(0));
+
+
+    let isDiscontinuousCharCode = -1;
+    for(let i = 0; i<charCodeArr.length; i++) {
+        if(charCodeArr[i]+1 !== charCodeArr[i+1]) {
+            isDiscontinuousCharCode=charCodeArr[i]
+            break;
+        }
+    }
+    return String.fromCharCode(isDiscontinuousCharCode+1)
+
+}
 
 module.exports = findMissingLetter;
+
+console.log(findMissingLetter(['d','e','f','h']))

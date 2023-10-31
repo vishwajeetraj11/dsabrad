fe2b4d217077bf3370f3d20b3f84929e6b409019
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 15:26:06 +0530

    phone directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneArr) {
+    const map = new Map();
+    phoneArr.forEach(phone => {
+        const [name, _phone] = phone.split(':');
+        map.set(name, _phone)
+    })
+    return map;
+}
 
 module.exports = phoneNumberDirectory;

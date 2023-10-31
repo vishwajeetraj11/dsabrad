b7fce4269cc436c60440db3e1035b625b77fb46b
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 14:21:40 +0530

    sliding window :

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,22 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+    let maxSum = -Infinity;
+    let currentSum = 0
+    for(let i=0; i<k;i++) {
+        currentSum += arr[i];
+    }
+
+    maxSum= currentSum
+
+    for(let i=k;i< arr.length;i++) {
+        currentSum =currentSum-arr[i-k] +arr[i];
+        maxSum = Math.max(maxSum, currentSum)
+    }
+    
+     return maxSum
+}
+
+const arr = [2, 5, 3, 1, 11, 7, 6, 4];
+const result = maxSubarraySum(arr, 3);
+// console.log(result);
 
 module.exports = maxSubarraySum;

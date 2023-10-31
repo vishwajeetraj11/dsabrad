a7929f950463c34cf3edb150297c8e8d9e8ddbd7
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 15:23:33 +0530

    word counter frequency

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,15 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(string) {
+    const map=new Map();
+    const arr = string.toLowerCase().split(/\W+/);
+    arr.forEach((word) => {
+        if (word === '') return;
+        if(map.has(word)) {
+            map.set(word, map.get(word)+1)
+        } else {
+            map.set(word, 1)
+        }
+    })
+    return map;
+}
 
 module.exports = wordFrequencyCounter;

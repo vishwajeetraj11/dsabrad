cf55fec44dbb518e4df27411d30096874d7322f4
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-31 13:42:39 +0530

    max sub array sum

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,15 @@
-function maxSubarraySum() {}
+function maxSubarraySum(array, k) {
+    let maxSum=-Infinity;
+    for(let i =0; i<=array.length-k;i++) {
+       let currentSum = 0;
+    for(let j =i; j<i+k;j++) {
+        currentSum = currentSum + array[j]
+    }
+    maxSum = Math.max(maxSum, currentSum);
+}
+    return maxSum
+}
+const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4]
+const result = maxSubarraySum(arr2, 4);
 
 module.exports = maxSubarraySum;

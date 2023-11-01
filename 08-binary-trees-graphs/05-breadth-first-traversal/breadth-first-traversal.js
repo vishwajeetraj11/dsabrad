61219e42f833622de43c7aaa0ccd68a69ffc01a4
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 18:53:31 +0530

    [tree]:bfs

--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,31 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left=null;
+    this.right=null;
+  }
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+  if(!root) return [];
+  const result = [];
+  const queue = [];
+  queue.push(queue)
+  while(queue.length!==0) {
+   const current = queue.shift();
+   result.push(current.data);
+   if(current.right){
+    queue.push(current.right)
+   } 
+   if(current.left) {
+    queue.push(current.left)
+   }
+  }
+
+  return result;
+}
 
 module.exports = {
   Node,

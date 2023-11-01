46f902c2a51103e770b3832b05a59930d80fd29c
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 15:43:46 +0530

    [queue]: implementation

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,43 @@
-class Queue {}
+class Queue {
+    
+    constructor() {
+        this.queue = [];
+        this.head = 0
+        this.tail = 0
+        this.maxSize = 100;
+    }
+
+    enqueue(value) {
+        if(this.isFull()) return false;
+        this.queue[this.tail] = value;
+        this.tail++;
+        return true;
+    }
+
+    isEmpty() {
+        return this.getLength() === 0;
+    }
+
+    isFull() {
+        return this.getLength() === this.maxSize
+    }
+
+    getLength() {
+        return this.tail - this.head;
+    }
+
+    dequeue() {
+    //     this.head--;
+    //    return this.queue.pop()
+    const item = this.queue[this.head];
+    this.head++;
+    return item;
+    }
+
+    peek() {
+        return this.queue[this.head]
+    }
+
+}
 
 module.exports = Queue;

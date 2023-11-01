29cfdc0c5dcb65f6c1db74470c62ffc0f2755d97
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 18:12:35 +0530

    [linked-list]: implementation

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,13 @@
 const { LinkedList } = require('./linked-list');
+
+const list = new LinkedList()
+
+list.add(2)
+list.add(44)
+list.printAll()
+
+console.log('LL get', list.get(1))
+
+list.insertAt(1, 800)
+list.removeFrom(1)
+list.printAll()
--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -1,5 +1,81 @@
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    add(data) {
+        const node = new Node(data)
+        if(this.head === null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+        this.tail = node;
+    }
+
+    printAll() {
+        let current = this.head;
+        while(current !== null) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+
+    get(index) {
+        let current = this.head;
+        let i = 0;
+        while(i<index) {
+            current = current.next;
+            i++;
+        }
+        return current.data;
+    }
+
+    insertAt(index, data) {
+        const node = new Node(data);
+        if(index === 0) {
+            node.next = this.head;
+            this.head = node;
+        } else {
+            let current = this.head;
+            let previous = null;
+            let i=0;
+            while(i<index) {
+                previous = current;
+                current = current.next;
+                i++;
+            }
+            node.next = current;
+            previous.next = node;
+        }
+    }
+
+    removeFrom(index) {
+        
+        if(index === 0) {
+            this.head = this.head.next;
+        } else {
+            let current = this.head;
+            let previous = null;
+            let i=0;
+            while(i<index) {
+                previous = current;
+                current = current.next;
+                i++;
+            }
+            
+            previous.next = current.next;
+
+        }
+    }
+}
 
 module.exports = { Node, LinkedList };

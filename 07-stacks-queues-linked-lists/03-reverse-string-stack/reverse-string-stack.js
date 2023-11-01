de0fd170e6437c540f5bd7e4041b87eb82e5f5df
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 14:44:56 +0530

    [stack]: reverse string

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,14 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(string) {
+    const stack = new Stack();
+    let reverseString = ''
+    const stringArr = string.split('')
+    stringArr.forEach((letter => stack.push(letter)));
+    stringArr.forEach(letter => {
+        reverseString=reverseString+stack.pop();
+    })
+    return reverseString;   
+}
 
 module.exports = reverseStringStack;

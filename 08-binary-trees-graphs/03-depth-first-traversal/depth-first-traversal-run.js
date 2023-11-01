0b9e4ee68d46fe6ebff6a2bb134321e4ee394d99
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 18:49:19 +0530

    [tree]: dfs

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
@@ -6,3 +6,19 @@ const { Node, depthFirstTraversal } = require('./depth-first-traversal');
 //                        / \    /
 //                       d   e  f
 
+const root = new Node('a');
+
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+root.left = b;
+b.left = d
+b.right = e
+root.right = c
+c.left = f
+
+const result = depthFirstTraversal(root);
+console.log(result);
--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -1,8 +1,34 @@
 const Stack = require('./stack');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(root) {
+  if(!root) return result;
+  
+  const result = [];
+  const stack = [];
+
+  stack.push(root);
+
+  while(stack.length > 0) {
+    const current = stack.pop();
+    result.push(current.data);
+    if(current.right) {
+      stack.push(current.right)
+    }
+    if(current.left) {
+      stack.push(current.left)
+    }
+  }
+
+  return result;
+}
 
 module.exports = {
   Node,

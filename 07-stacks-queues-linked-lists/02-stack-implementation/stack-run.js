269713f53ee1f1b1d269e9558a9e2f8b107e6fbd
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 14:31:59 +0530

    Stack: implementation push pop

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
@@ -1,2 +1,10 @@
 const Stack = require('./stack');
 
+const stack = new Stack();
+stack.push(1)
+stack.push(2)
+stack.push(5)
+
+stack.pop()
+
+console.log(stack)
--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,35 @@
-class Stack {}
+class Stack {
+
+    constructor() {
+        this.maxSize = 100;
+        this.stack = [];
+        this.top = -1;
+    }
+
+    push(value) {
+        if(this.isFull()) return false;
+        this.top++;
+        this.stack[this.top] = value;
+        return true;
+    }
+
+    pop() {
+        // If stack empty.
+        if(this.isEmpty()) return null;
+        this.top--;
+        return this.stack.pop();;
+    }
+
+    isEmpty() {
+        return this.top === -1;
+    }
+
+    isFull() {
+        return this.top === this.maxSize - 1;
+    }
+
+    
+
+}
 
 module.exports = Stack;

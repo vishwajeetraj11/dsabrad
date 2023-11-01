b3dc2775a95c6f2076fa7f2e8ffcd9fe38a6e8a6
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-01 15:59:32 +0530

    [queue]: rev string

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,23 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+    const queue = new Queue();
+
+    for(let i = 0; i<str.length; i++) {
+        queue.enqueue(str[i])
+    }
+
+    // 1.
+    let revString = '';
+    // for(let i = 0; i<str.length; i++) {
+    //     revString =queue.dequeue()+revString
+    // }
+    // return revString
+
+    while(!queue.isEmpty()) {
+        revString=queue.dequeue()+revString
+    }
+    return revString
+};
 
 module.exports = reverseStringWithQueue;

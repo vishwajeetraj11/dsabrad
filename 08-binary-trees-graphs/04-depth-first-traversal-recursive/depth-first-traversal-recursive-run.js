389fc83d323965d1619b44f10fbc6392578f6afa
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-03 13:00:14 +0530

    dfs recursive

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive-run.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive-run.js
@@ -1,4 +1,4 @@
-const { Node, depthFirstTraversal } = require('./depth-first-traversal');
+const { Node, recDepthFirstTraversal } = require('./depth-first-traversal-recursive');
 
 // Create a binary tree:      a
 //                          /   \
@@ -6,3 +6,11 @@ const { Node, depthFirstTraversal } = require('./depth-first-traversal');
 //                        / \    /
 //                       d   e  f
 
+const root = new Node('a');
+root.left = new Node('b');
+root.right = new Node('c');
+root.left.left = new Node('d');
+root.left.right = new Node('e');
+root.right.left = new Node('f');
+
+console.log(recDepthFirstTraversal(root))
--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,6 +1,32 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function recDepthFirstTraversal() {}
+function recDepthFirstTraversal(root) {
+  const result=[];
+ 
+  function traverse(node) {
+    
+    if(node!==null) {
+      const data = node.data;
+      result.push(data)
+      if(node.left) {
+        traverse(node.left);
+      }
+      if(node.right){
+         traverse(node.right);
+        }
+    }
+  }
+
+  traverse(root)
+
+  return result 
+}
 
 module.exports = {
   Node,

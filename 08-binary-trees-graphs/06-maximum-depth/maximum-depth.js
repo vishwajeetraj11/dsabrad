80c556b6acca11db18983003c21f4291b519f937
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-03 13:14:17 +0530

    [tree]: max depth

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,21 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+  
+   if(root === null) return [];
+
+  const leftRoot = maxDepth(root.left);
+  const rightRoot = maxDepth(root.right);
+
+  return Math.max(leftRoot, rightRoot)+1
+
+}
 
 module.exports = {
   maxDepth,

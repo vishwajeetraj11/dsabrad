4b532022b2f25fb02f33793c428adefd739bc34d
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-06 21:09:54 +0530

    Add validation for binary search tree

--- 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
@@ -7,3 +7,15 @@ const { Node, isValidBST } = require('./validate-bst');
 //    / \
 //   2   6
 
+const root = new Node(8);
+const node10 = new Node(10);
+const node4 = new Node(4);
+const node2 = new Node(2);
+const node6 = new Node(6);
+
+root.left = node4;
+root.right = node10;
+node4.left = node2;
+node4.right=node6;
+
+const isValid = isValidBST(root);
--- 08-binary-trees-graphs/09-validate-bst/validate-bst.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst.js
@@ -6,6 +6,18 @@ class Node {
   }
 }
 
-function isValidBST() {}
+function isValidBST(root) {
+  function isValidSubTree(node, min,max) {
+    if(!node) return true;
+
+    if((min!==null && node.value <= min) || (max!== null && node.value >= max)) {
+      return false;
+    }
+
+    return isValidSubTree(node.left, min, node.value) && isValidSubTree(node.right, node.data, max)
+  }
+
+  return isValidSubTree(root, null, null)
+}
 
 module.exports = { Node, isValidBST };

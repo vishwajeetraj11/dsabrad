a2df4292f4a83def76733d387de1812e6c2f3b9f
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-08 22:18:14 +0530

    Refactored lengthOfLastWord function to improve efficiency

--- dsa/questions/lengthOfLastWord.js
+++ dsa/questions/lengthOfLastWord.js
@@ -1,11 +1,25 @@
 // https://leetcode.com/problems/length-of-last-word/
 
-const lengthOfLastWord = (str) => {
-    const trimmedString = str.trim();
-    const strArr = trimmedString.split(' ').filter(Boolean);
-    return strArr[strArr.length-1].length
-}
+// const lengthOfLastWord = (str) => {
+//     const trimmedString = str.trim();
+//     const strArr = trimmedString.split(' ').filter(Boolean);
+//     return strArr[strArr.length-1].length
+// }
 
+const lengthOfLastWord = (str) => {
+    let count = 0;
+    for(let i=str.length-1;i>=0;i--) {
+         if(str[i] !== ' ') {
+             count++;
+         } else if(count === 0) {
+            continue
+         } else {
+            break;
+         }
+    }
+ 
+    return count;
+ }
 console.log(lengthOfLastWord('Hello World'))
 
 console.log(lengthOfLastWord("   fly me   to   the moon  "))

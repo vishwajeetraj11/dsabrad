994489b0246ebc8aeec0683d78dcc09d57b6bd76
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-16 00:13:29 +0530

    merge intervals

---
+++ dsa/questions/mergeIntervals.js
@@ -0,0 +1,73 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+// wrong solution comparing only two intervals
+// console.log(merge([[1,4],[0,2],[3,5]])) fails op=[ [ 0, 4 ], [ 1, 5 ] ]
+
+// var merge = function(intervals) {
+//     let l=0; r=1;
+//     const final = []
+//     if(intervals.length === 1) return intervals
+//     intervals.sort((a,b) => a[0]-b[0])
+//     let flag = false;
+//     while(r<intervals.length) {
+//         const leftE = intervals[l];
+//         const rightE = intervals[r];
+//         console.log(leftE)
+//         const [c,d] = leftE;
+//         const [f,g] = rightE;
+//         const left = Math.min(c,d,f,g);
+//         const right = Math.max(d,g);
+//         if(f<d || f===d) {
+//             // final.push([c,d>g?d:g])
+//             final.push([left,right])
+//             flag = true;
+//         } else {
+//             if(!flag) {
+//                 final.push(leftE)
+//             }
+//             final.push(rightE)
+//             flag = true;
+//         }
+//         l++;
+//         r++;
+//     }
+//     return final
+// };
+
+var merge = function(intervals) {
+    if(intervals.length === 1) return intervals
+    intervals.sort((a,b) => a[0]-b[0]);
+    let curr = intervals[0];
+    let final = [curr];
+    let flag = false;
+    for(let i=1; i<intervals.length; i++) {
+        const [cf,cl] = curr;
+        const [lf, ll] = intervals[i];
+        if(cl >= lf) {
+            curr = [cf, cl>ll?cl:ll]
+        } else {
+            final.push(intervals[i])
+            curr = intervals[i]
+        }
+
+        if(final.length === 1) {
+            final = [curr];
+        } else {
+            final.pop();
+            final.push(curr)
+        }
+    }
+    return final
+};
+
+
+
+
+console.log(merge([[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]))
+// console.log(merge([[1,4],[2,3]]))
+// console.log(merge([[0,2],[1,4],[3,5]]))
+// console.log(merge([[1,3],[2,6],[8,10],[15,18]]))
+// console.log(merge([[1,4],[4,5]]))
+// console.log(merge([[1,4],[5,6]]))

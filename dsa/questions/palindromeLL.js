9a90f213cf849e21a8456dc0403e8c55d08b2970
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-01 20:37:05 +0530

    Palindrome LL

--- dsa/questions/palindromeLL.js
+++ dsa/questions/palindromeLL.js
@@ -11,21 +11,59 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    if(head === null) {
-            return true
-        }
-        const arr=[];
-        let current = head;
-        while(current!==null) {
-            arr.push(current.val)
-            current = current.next;
-        }
+// bruteforce
+// var isPalindrome = function(head) {
+//     if(head === null) {
+//             return true
+//         }
+//         const arr=[];
+//         let current = head;
+//         while(current!==null) {
+//             arr.push(current.val)
+//             current = current.next;
+//         }
         
-        for(let i=0;i<arr.length;i++) {
-            if(arr[i] !== arr[arr.length-1-i]) {
-                return false;
-            }
+//         for(let i=0;i<arr.length;i++) {
+//             if(arr[i] !== arr[arr.length-1-i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+// };
+
+const reverseLL = (head) => {
+    let curr = head;
+    let next = null, prev = null;
+    while(curr) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+const isPalindrome = (head) => {
+    let fast = head;
+    let slow = head;
+
+    let pointer = head;
+    let length = 0;
+
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        len++;
+    }
+
+    let mid = reverseLL(slow); 
+
+    while(length) {
+        length--;
+        if(mid.val !== pointer.val) {
+            return false;
         }
-        return true;
-};
+        mid = mid.next;
+        pointer = pointer.next;
+    }
+    return true;
+}

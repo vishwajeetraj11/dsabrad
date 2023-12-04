d10a240df2a71898a41fb251498cfb3c20520669
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-05 00:43:10 +0530

    diameter of binary tree

---
+++ dsa/questions/diameterOfBinaryTree.js
@@ -0,0 +1,18 @@
+// 543. Diameter of Binary Tree
+/* 
+https://leetcode.com/problems/diameter-of-binary-tree/
+*/
+
+const diameterOfBinaryTree = (root) => {
+    if(!root) return 0;
+    let max = 0;
+    const dfs = (node) => {
+        if(!node) return 0;
+        let left = dfs(node.left);
+        let right = dfs(node.right);
+        max = Math.max(left+right, max);
+        return Math.max(left, right)+1;
+    }
+    dfs(root);
+    return max;
+}

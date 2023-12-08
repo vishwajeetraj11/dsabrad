7ba3058120b2378e98011dfdd0275bf306dcd36f
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-09 00:24:53 +0530

    update.

--- dsa/questions/sum3.js
+++ dsa/questions/sum3.js
@@ -24,7 +24,7 @@ const threeSum = (nums) => {
                 if(partialSum > target) {
                     k--;
                 } else {
-                    j++
+                    j++;
                 }
             }
         }

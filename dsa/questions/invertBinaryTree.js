8435a896fea1f3f9ea1aa689e3415eb1fcf39d50
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-10 00:45:15 +0530

    226. Invert Binary Tree

---
+++ dsa/questions/invertBinaryTree.js
@@ -0,0 +1,21 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root) {
+        [root.left, root.right] = [invertTree(root.right), invertTree(root.left)]
+    }
+    return root;
+};
